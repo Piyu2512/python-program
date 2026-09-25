f5ce15d14cf3f2cd8f75b95c7418d38efb7d3c2a
Piyu2512 <priyanshishukla2522@gmail.com>
2026-09-25 08:13:35 +0530

    Add files via upload

---
+++ arr.py
@@ -0,0 +1,22 @@
+from numpy import *
+
+a = array([1, 2, 3, 4, 5], int)
+print(a)
+
+
+
+a = array([1, 2, 3, 4, 5], int)
+print(a)
+
+
+a = array(['amar', 'bimal', 'ceema', 'dipak', 'rita'], dtype=str)
+print(a)
+
+
+
+a = array(['a', 'b', 'c', 'd', 'e'])
+print(a)
+
+
+a=linespace(1,5,5)
+print(a)
---
+++ arr2.py
@@ -0,0 +1,8 @@
+from numpy import *
+
+a = linspace(1, 5, 5)
+print(a)
+
+from numpy import *
+a=logspace (1,5,5)
+print(a)
